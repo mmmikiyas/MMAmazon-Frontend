cb74099649f208bdb22e370c7e0133699af4a1c0
Mikiyas Mekonnen Teklemariam <52347707+mmmikiyas@users.noreply.github.com>
2026-01-29 14:22:09 -0800

    Wrap App component with BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
-
-
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import DataProvider from './Components/DataProvider/DataProvider'
-import{initialState,reducer} from './Utility/reducer'
+import DataProvider from './Components/DataProvider/DataProvider';
+import { initialState, reducer } from './Utility/reducer';
+import { BrowserRouter } from 'react-router-dom'; // <-- import BrowserRouter
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <DataProvider reducer={reducer} initialState={initialState}>
-    <App />
+      <BrowserRouter basename="/MMAmazon-Frontend"> {/* <-- set basename */}
+        <App />
+      </BrowserRouter>
     </DataProvider>
   </React.StrictMode>
 );
-
-
